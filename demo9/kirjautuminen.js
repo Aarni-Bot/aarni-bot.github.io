40a08f66e79597fb951a5a54c34a67ebbac8a7d5
Aarni Annanolli <Aarninko@gmail.com>
2026-02-10 22:32:02 +0200

    Kirjautumislomake  päivitys

--- demo9/kirjautuminen.js
+++ demo9/kirjautuminen.js
@@ -1,32 +1,34 @@
 document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 
-
 function onkoKirjautunut() {
     if (localStorage.getItem("kirjautunut") == "kyllä") {
         document.getElementById("kirjaudu_nappi").style.display = "none";
-        document.getElementById("kirjaudu_ulos_nappi").style.display = "visible";
-        document.getElementById("welcome-text").textContent += localStorage.getItem("nimi");
+        document.getElementById("kirjaudu_ulos_nappi").style.display = "inline-block";
+        document.getElementById("welcome-text").textContent = "Tervetuloa " + localStorage.getItem("nimi");
         document.getElementById("nimi").disabled = true;
-        document.getElementById("nimi").placeholder = localStorage.getItem("nimi");
-
-    } else if (localStorage.getItem("kirjautunut") == "ei") {
+        document.getElementById("nimi").value = localStorage.getItem("nimi");
+    } else {
         document.getElementById("kirjaudu_ulos_nappi").style.display = "none";
-        document.getElementById("nimi").style.display = "visible";
-        document.getElementById("nimi").disabled = false
+        document.getElementById("kirjaudu_nappi").style.display = "inline-block";
+        document.getElementById("nimi").disabled = false;
+        document.getElementById("nimi").value = "";
         document.getElementById("nimi").placeholder = "Syötä nimesi";
-
+        document.getElementById("welcome-text").textContent = "Tervetuloa";
     }
 }
 
-
 function kirjaudu() {
+    event.preventDefault();
     if (document.getElementById("nimi").value != "") {
         localStorage.setItem("nimi", document.getElementById("nimi").value);
-        localStorage.setItem("kirjautunut", "kyllä")
+        localStorage.setItem("kirjautunut", "kyllä");
+        onkoKirjautunut();
     }
 }
 
 function kirjauduUlos() {
-    localStorage.setItem("kirjautunut", "ei")
-    localStorage.nimi = ""
+    event.preventDefault();
+    localStorage.setItem("kirjautunut", "ei");
+    localStorage.setItem("nimi", "");
+    onkoKirjautunut();
 }
